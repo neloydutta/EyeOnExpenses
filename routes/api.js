e22caec4ccbe794ce5de2c54493697f6daa67d2f
neloydutta <neloy.dutta@live.com>
2017-03-08 17:25:03 +0530

    store-transfer-api

--- routes/api.js
+++ routes/api.js
@@ -8,7 +8,7 @@ var expense = mongoose.model('expense', dbsetup.expenseSchema);
 
 
 router.get('/store-add-balance', function(req, res){
-    query = req.query;
+    query = req.query; //Query string: username=&store=&amt=
     //console.log(query)
     if(query.store && query.amt && query.username){
         if(dbsetup.status){
@@ -17,14 +17,14 @@ router.get('/store-add-balance', function(req, res){
                     console.log('DB Fetch error!');
                     console.log(err);
                     res.statusMessage = 'Oops!';
-                    res.status(400).json({'status': 'failure'});
+                    res.status(500).json({'status': 'failure', 'message': 'internal server error'});
                 }
                 else{
                     if(length(store_res) > 0){
-                        for(i=0; i<length(store_res.stores); i++){
-                            if(store_res.stores[i].name == query.store){
+                        for(i=0; i<length(store_res[0].stores); i++){
+                            if(store_res[0].stores[i].name == query.store){
                                 //store_list[i].balance += parseInt(query.amt);
-                                store_res.stores[i].balance += parseInt(query.amt);
+                                store_res[0].stores[i].balance += parseInt(query.amt);
                                 store_res.visits.$inc();
                                 store_res.save()
                                 break;
@@ -33,7 +33,7 @@ router.get('/store-add-balance', function(req, res){
                         if(i == length(store_list)){
                             console.log('DB Fetch error! (Probable cause: Invalid store)');
                             res.statusMessage = 'Oops!';
-                            res.status(400).json({'status': 'failure'});
+                            res.status(400).json({'status': 'failure', 'message': 'invalid store'});
                         }
                         else{
                             res.statusMessage = 'success!';
@@ -43,7 +43,7 @@ router.get('/store-add-balance', function(req, res){
                     else{
                         console.log('DB Fetch error! (Probable cause: Invalid username)');
                         res.statusMessage = 'Oops!';
-                        res.status(400).json({'status': 'failure'});
+                        res.status(400).json({'status': 'failure', 'message': 'invalid username'});
                     }
                 }
             });
@@ -51,18 +51,18 @@ router.get('/store-add-balance', function(req, res){
         else{
             console.log('DB setup error!');
             res.statusMessage = 'Oops!';
-            res.status(500).json({'status': 'failure'});
+            res.status(500).json({'status': 'failure', 'message': 'internal server error'});
         }
     }
     else{
-        console.log('Bad Request by user!')
+        console.log('Bad Request by user! (Probable cause: Insufficient Parameters)');
         res.statusMessage = 'Oops!';
-        res.status(400).json({'status': 'failure'});
+        res.status(400).json({'status': 'failure', 'message': 'insufficient parameters'});
     }
 });
 
 router.get('/store-subtract-balance', function(req, res){
-    query = req.query;
+    query = req.query; //Query string: username=&store=&amt=
     //console.log(query)
     if(query.store && query.amt && query.username){
         if(dbsetup.status){
@@ -71,18 +71,18 @@ router.get('/store-subtract-balance', function(req, res){
                     console.log('DB Fetch error!');
                     console.log(err);
                     res.statusMessage = 'Oops!';
-                    res.status(400).json({'status': 'failure'});
+                    res.status(500).json({'status': 'failure', 'message': 'internal server error'});
                 }
                 else{
                     if(length(store_res) > 0){
                         for(i=0; i<length(store_res.stores); i++){
-                            if(store_res.stores[i].name == query.store){
+                            if(store_res[0].stores[i].name == query.store){
                                 //store_list[i].balance += parseInt(query.amt);
-                                store_res.stores[i].balance -= parseInt(query.amt);
+                                store_res[0].stores[i].balance -= parseInt(query.amt);
                                 if(store_res.stores[i].balance < 0){
                                     console.log('Bad Request! (Probable cause: Store balance less => -ve)');
                                     res.statusMessage = 'Oops!';
-                                    res.status(400).json({'status': 'failure'});
+                                    res.status(400).json({'status': 'failure', 'message': 'insufficient store balance'});
                                 }
                                 else{
                                     store_res.visits.$inc();
@@ -96,13 +96,13 @@ router.get('/store-subtract-balance', function(req, res){
                         if(i == length(store_list)){
                             console.log('DB Fetch error! (Probable cause: Invalid store)');
                             res.statusMessage = 'Oops!';
-                            res.status(400).json({'status': 'failure'});
+                            res.status(400).json({'status': 'failure', 'message': 'invalid store'});
                         }
                     }
                     else{
                         console.log('DB Fetch error! (Probable cause: Invalid username)');
                         res.statusMessage = 'Oops!';
-                        res.status(400).json({'status': 'failure'});
+                        res.status(400).json({'status': 'failure', 'message': 'invalid username'});
                     }
                 }
             });
@@ -110,20 +110,56 @@ router.get('/store-subtract-balance', function(req, res){
         else{
             console.log('DB setup error!');
             res.statusMessage = 'Oops!';
-            res.status(500).json({'status': 'failure'});
+            res.status(500).json({'status': 'failure', 'message': 'Internal server error'});
         }
     }
     else{
         console.log('Bad Request by user! (probable cause: insufficient parameters)')
         res.statusMessage = 'Oops!';
-        res.status(400).json({'status': 'failure'});
+        res.status(400).json({'status': 'failure', 'message': 'insufficient parameters'});
     }
 });
 
 router.get('/store-transfer-balance', function(req, res){
-    query = req.query;
+    query = req.query; //Query string: tostore-&fromstore=&amt=&username=
     if(query.tostore && query.fromstore && query.amt && query.username){
-
+        if(dbsetup.status){
+            store.find({username: query.username}, function(err, store_res){
+                tostoreidx = -1
+                for(i=0; i<length(store_res.stores); i++){
+                    if(store_res[0].store[i].name == query.tostore){
+                        tostoreidx = i;
+                        continue;
+                    }
+                    else if(store_res[0].stores[i].name == query.fromstore){
+                        if(store_res[0].stores[i].balance >= query.amt){
+                            store_res[0].store[i].balance -= query.amt
+                            if(tostoreidx == -1){
+                                for(j=0; j<length(store_res.stores); j++){
+                                    if(store_res[0].stores[j].name == query.tostore){
+                                        tostoreidx = j;
+                                        break;
+                                    }
+                                }
+                            }
+                            store_res[0].stores[tostoreidx].balance += query.amt;
+                            store_res.visits.$inc();
+                            store_res.save();
+                        }
+                        else{
+                            console.log('Bad request by user! (Probable cause: Insufficient balance)');
+                            res.statusMessage = 'Oops!';
+                            res.status(400).json({'status': 'failure', 'message': 'insufficient balance'});
+                        }
+                    }
+                }
+            });
+        }
+        else{
+            console.log('DB setup error!');
+            res.statusMessage = 'Oops!';
+            res.status(500).json({'status': 'failure', 'message': 'internal server error'});
+        }
     }
     else{
         console.log('Bad request by user! (probable cause: insufficient parameters)');
